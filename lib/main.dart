b12ab33eff61240fad6287266b585be216f951b1
arthuralvesdl <arthuralveslacerda@gmail.com>
2024-04-26 12:22:27 -0300

    style: theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutte_travel_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Flutter Travel UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: Color(0xFF3EBACE),
+              onPrimary: Color(0xFF3EBACE),
+              secondary: Color(0xFFD8ECF1),
+              onSecondary: Color(0xFFD8ECF1),
+              error: Colors.red,
+              onError: Colors.red,
+              background: Color(0xFFF3F5F7),
+              onBackground: Color(0xFFF3F5F7),
+              surface: Colors.blue,
+              onSurface: Colors.blue)),
+      home: const HomeScreen(),
     );
   }
 }
